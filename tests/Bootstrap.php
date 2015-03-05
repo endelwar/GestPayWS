5dca9edb17b569a643ea93169bc75a21db6a46de
Manuel Dalla Lana <endelwar@aregar.it>
2015-03-05 12:06:14 +0100

    Adding license to test files

---
+++ tests/Bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * This file is part of the GestPayWS library.
+ *
+ * (c) Manuel Dalla Lana <endelwar@aregar.it>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
--- tests/Parameter/EncryptParameterTest.php
+++ tests/Parameter/EncryptParameterTest.php
@@ -1,9 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: manuel
- * Date: 05/03/15
- * Time: 07:51
+/*
+ * This file is part of the GestPayWS library.
+ *
+ * (c) Manuel Dalla Lana <endelwar@aregar.it>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace EndelWar\GestPayWS\Parameter\Test;
