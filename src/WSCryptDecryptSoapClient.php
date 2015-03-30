583f1810b31bb8b90f3d597fe7af6a07a29520d7
Manuel Dalla Lana <endelwar@aregar.it>
2015-03-30 18:52:13 +0200

    limit ssl ciphers to the ones supported by testecomm.sell.it
    the only secure one (for now) is DES-CBC3-SHA

--- src/WSCryptDecryptSoapClient.php
+++ src/WSCryptDecryptSoapClient.php
@@ -17,10 +17,23 @@ class WSCryptDecryptSoapClient
         'production' => 'https://ecomms2s.sella.it/gestpay/GestPayWS/WsCryptDecrypt.asmx?wsdl'
     );
     public $wsdlEnvironment;
+
+    /**
+     * As of 30 march 2015 test site supports the following ssl ciphers:
+     * TLS_RSA_WITH_RC4_128_MD5 (0x4)
+     * TLS_RSA_WITH_RC4_128_SHA (0x5)
+     * TLS_RSA_WITH_3DES_EDE_CBC_SHA (0xa)
+     * TLS_RSA_WITH_DES_CBC_SHA (0x9)
+     * TLS_RSA_EXPORT1024_WITH_RC4_56_SHA (0x64)
+     * TLS_RSA_EXPORT1024_WITH_DES_CBC_SHA (0x62)
+     * TLS_RSA_EXPORT_WITH_RC4_40_MD5 (0x3)
+     * TLS_RSA_EXPORT_WITH_RC2_CBC_40_MD5 (0x6)
+     *
+     * The only one which is secure is TLS_RSA_WITH_3DES_EDE_CBC_SHA (called DES-CBC3-SHA in openssl)
+     */
     protected $streamContextOption = array(
         'ssl' => array(
-            'ciphers' => 'DHE-RSA-AES256-SHA:DHE-DSS-AES256-SHA:AES256-SHA:KRB5-DES-CBC3-MD5:KRB5-DES-CBC3-SHA:EDH-RSA-DES-CBC3-SHA:EDH-DSS-DES-CBC3-SHA:DES-CBC3-SHA:DES-CBC3-MD5:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA:AES128-SHA:RC2-CBC-MD5:KRB5-RC4-MD5:KRB5-RC4-SHA:RC4-SHA:RC4-MD5:RC4-MD5:KRB5-DES-CBC-MD5:KRB5-DES-CBC-SHA:EDH-RSA-DES-CBC-SHA:EDH-DSS-DES-CBC-SHA:DES-CBC-SHA:DES-CBC-MD5:EXP-KRB5-RC2-CBC-MD5:EXP-KRB5-DES-CBC-MD5:EXP-KRB5-RC2-CBC-SHA:EXP-KRB5-DES-CBC-SHA:EXP-EDH-RSA-DES-CBC-SHA:EXP-EDH-DSS-DES-CBC-SHA:EXP-DES-CBC-SHA:EXP-RC2-CBC-MD5:EXP-RC2-CBC-MD5:EXP-KRB5-RC4-MD5:EXP-KRB5-RC4-SHA:EXP-RC4-MD5:EXP-RC4-MD5',
-            'disable_compression' => true,
+            'ciphers' => 'DES-CBC3-SHA:RC4-SHA:RC4-MD5',
             'SNI_enabled' => true,
             'verify_peer' => true,
             'verify_depth' => 5,
@@ -56,7 +69,10 @@ class WSCryptDecryptSoapClient
     private function setTestEnvironment()
     {
         $this->wsdlEnvironment = 'test';
-        $soapClientTestOption = array('trace' => true);
+        $soapClientTestOption = array(
+            'trace' => true,
+            'cache_wsdl' =>  WSDL_CACHE_NONE
+        );
 
         return $soapClientTestOption;
     }
