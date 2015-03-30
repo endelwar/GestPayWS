dfa8c6489a0bea78e4e28ff98ad6dbc14f582e39
Manuel Dalla Lana <endelwar@aregar.it>
2015-03-30 17:54:39 +0200

    adding initial version of Parameter\DecryptParameter

---
+++ src/Parameter/DecryptParameter.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of the GestPayWS library.
+ *
+ * (c) Manuel Dalla Lana <endelwar@aregar.it>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace EndelWar\GestPayWS\Parameter;
+
+/**
+ * Class DecryptParameter
+ * @package EndelWar\GestPayWS\Parameter
+ */
+class DecryptParameter extends Parameter
+{
+    protected $parametersName = array(
+        'shopLogin',
+        'CryptedString'
+    );
+
+    protected $mandatoryParameters = array(
+        'shopLogin',
+        'CryptedString'
+    );
+}
