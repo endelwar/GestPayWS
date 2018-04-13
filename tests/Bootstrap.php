73eaf86540e872a62289b0b963dc5e8468169e31
Manuel Dalla Lana <manuel@pepeverde.agency>
2018-04-13 16:55:53 +0200

    Update third party test config

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -15,3 +15,10 @@ if (function_exists('date_default_timezone_set') && function_exists('date_defaul
 }
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+// PHPUnit 6 introduced a breaking change that
+// removed PHPUnit_Framework_TestCase as a base class,
+// and replaced it with \PHPUnit\Framework\TestCase
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
